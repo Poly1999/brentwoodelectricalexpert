c56eae3ce91724d026b7532e783f5d9dad081a71
Poly1999 <pikalova.developer@gmail.com>
2026-09-22 16:10:16 -0400

    fix: debug link in about page

--- src/components/about-page/AboutHero/AboutHero.tsx
+++ src/components/about-page/AboutHero/AboutHero.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import './AboutHero.css';
 import { Phone, FileText } from 'lucide-react';
 
@@ -21,10 +22,10 @@ function AboutHero() {
               <Phone />
               Call (310) 666-4752
             </a>
-            <a href='/contact' className='abouthero_button_secondary'>
+            <NavLink to='/contact' className='abouthero_button_secondary'>
               <FileText />
               Get FREE Quote
-            </a>
+            </NavLink>
           </div>
         </div>
       </div>
